32edae3ddb9f1148fa4f24bb787089b3973d54c5
elihl <elihl@LAPTOP-EVR5KTER>
2023-07-13 23:30:02 +0200

    Add project files.

---
+++ Enumeration/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enumeration
+{
+    internal class Program
+    {
+        enum Month
+        {
+            January=1,February,March,April,May,June,July,August,September,October,November,December
+        }
+        static void Main(string[] args)
+        {
+            Month month;
+            Console.Write("Enter a month Number:");
+            int pos=int.Parse(Console.ReadLine());
+            month = (Month)pos;
+            Console.WriteLine("{0} is month {1}", month, pos);
+            Console.ReadLine();
+        }
+    }
+}
